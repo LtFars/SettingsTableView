03b5051caa9836757fb65e1f630acbc59d28240d
Denis Snezhko <mikle4421@mail.ru>
2021-10-06 13:10:21 +0300

    Creating settings for each cell.

--- SettingsTableView/ViewController.swift
+++ SettingsTableView/ViewController.swift
@@ -1,31 +1,52 @@
 import UIKit
 
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+}
+
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        cell.textLabel?.text = "cell \(indexPath[1])"
-        
-        return cell
-    }
     
     private let tableView: UITableView = {
         var table = UITableView(frame: .zero, style: .grouped)
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
         return table
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
         title = "Настройки"
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
+    }
+    
+    func configure() {
+        self.models = Array(0...10).compactMap({
+            SettingsOption(title: "Cell \($0)", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink) {
+                
+            }
+        })
+    }
+    
+    var models = [SettingsOption]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as? SettingsTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.configure(with: model)
+        
+        return cell
     }
 }
